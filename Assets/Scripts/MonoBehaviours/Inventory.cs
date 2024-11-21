34153acbfec70f85721eb15b4d9b406edff4095d
Logan Reine <log.a.of.n@gmail.com>
2024-11-21 11:21:37 -0500

    finished inventory object and scripts

--- Assets/Scripts/MonoBehaviours/Inventory.cs
+++ Assets/Scripts/MonoBehaviours/Inventory.cs
@@ -1,7 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
@@ -23,12 +21,78 @@ public class Inventory : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateSlots();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // Initialize all of the slots
+    public void CreateSlots()
+    {
+        // make sure that the slot prefab has been set in the unity editor
+        if (slotPrefab != null)
+        {
+            for (int i = 0; i < NUMSLOTS; i++)
+            {
+                // Create a new Slot game object and give it a name
+                GameObject newSlot = Instantiate(slotPrefab);
+                newSlot.name = "ItemSlot_" + i;
+
+                // set slot as the child of the InventoryBackground object
+                newSlot.transform.SetParent(gameObject.transform.GetChild(0).transform);
+
+                slots[i] = newSlot;
+
+                // get image component of the item
+                itemImages[i] = newSlot.transform.GetChild(1).GetComponent<Image>();
+            }
+        }
+    }
+
+    /**
+    * Addds an item to inventory
+    * itemToAdd: the item to be added to the inventory
+    * return: true if the item was successfully added
+    */
+    public bool AddItem(Item itemToAdd)
+    {
+        for (int i = 0; i < items.Length; i++)
+        {
+            // check to see if the current item in the index, if one exists, is one of the same type the player 
+            // wants to add, and see if it is a stackable item
+            if (items[i] != null && items[i].itemType == itemToAdd.itemType && itemToAdd.stackable == true)
+            {
+                // Increase teh quanity since it is a stackable item
+                items[i].quantity += 1;
+
+                // Grab a reference to the script that's in the slot (which contains a referencye to the QtyText)
+                Slot slotScript = slots[i].gameObject.GetComponent<Slot>();
+                Text quanitityText = slotScript.qtyText;
+
+                // Enable the text and set the text to be the quanity of stackable items
+                quanitityText.enabled = true;
+                quanitityText.text = items[i].quantity.ToString();
+            }
+
+            if (items[i] == null)
+            {
+                // Adding to empty slot
+                // This is the first item added to the array of a particular itemType, or the item isn't stackable
+
+                // Copy item & add to inventory.  Copying so we don't change the original scriptable object
+                items[i] = Instantiate(itemToAdd);
+                items[i].quantity = 1;
+                itemImages[i].sprite = itemToAdd.sprite;
+                itemImages[i].enabled = true;
+
+                return true;
+            }
+        }
+        // Inventory is full; cannot add new item
+        return false;
     }
 }
--- Assets/Scripts/MonoBehaviours/Player.cs
+++ Assets/Scripts/MonoBehaviours/Player.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class Player : Character
 {
     // Used to get a reference to the prefab
+    public Inventory inventoryPrefab;
+
+    // A copy of the inventory prefab
+    Inventory inventory;
+
+    // Used to get a reference to the prefab
     public HealthBar healthBarPrefab;
 
     // A copy of the health bar prefab
@@ -10,21 +16,34 @@ public class Player : Character
 
     public void Start()
     {
+        // Get a copy of the inventory prefab and store a reference to it
+        inventory = Instantiate(inventoryPrefab);
+
+        // Start teh player off with the starting hit point value
         hitPoints.value = startingHitPoints;
 
+        // Get a copy of the health bar prfefab and store a reference to it
         healthBar = Instantiate(healthBarPrefab);
+
+        // Set the healthBar's character property to this cahracter so it can retrieve the maxHitPoints
         healthBar.character = this;
     }
 
+    // Called when palyer's collider touches ans "Is Trigger" collider
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // trigger for any player collisions with consumable items
+        // Retrive the game object that the player collided with, and check the tag
         if (collision.gameObject.CompareTag("CanBePickedUp"))
         {
+
+            // Grab a reference to the Item (scriptable object) inside the Consumable class and assign it toe hitObject
             Item hitObject = collision.gameObject.GetComponent<Consumable>().item;
 
+            // Check for null to make sure it was successfully retrieved, and avoid potential errors
             if (hitObject != null)
             {
+                // indicates if the collision object should disappear
                 bool shouldDisappear = false;
 
                 // debugging
@@ -33,9 +52,13 @@ public class Player : Character
                 switch(hitObject.itemType)
                 {
                     case Item.ItemType.COIN:
+                        // coins will disappear if they can be added to the inventory
+                        shouldDisappear = inventory.AddItem(hitObject);
                         break;
 
                     case Item.ItemType.HEALTH:
+                        // hearts should disappear if they adjust the player's hit points
+                        // when health meter is full, hearts aren't picked up and remain in the scene
                         shouldDisappear = AdjustHitPoints(hitObject.quantity);
                         break;
                     
