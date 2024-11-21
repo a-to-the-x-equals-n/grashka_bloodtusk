eecb0f5d447a5b75f41a5dc3f291f3afba17f28d
Logan Reine <log.a.of.n@gmail.com>
2024-11-21 13:08:20 -0500

    added spawn points and camera manager

---
+++ Assets/Scripts/MonoBehaviours/RPGGameManager.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class RPGGameManager : MonoBehaviour
+{
+    // Reference to the camera manager class
+    public RPGCameraManager cameraManager;
+
+    // Reference to the spawn point designed for the player
+    // Needed so the player can be re-spawn when they die
+    public SpawnPoint playerSpawnPoint;
+
+    // A variable used to access the singleton object
+    public static RPGGameManager sharedInstance = null;
+
+    // Ensure only a single isntance of the RPGGameManager exists
+    // It's possibel to get multiple instances if multiple copies of the RPGGameManager exists in the Hierarchy
+    // or if multiple copies are programmatically instantiated
+    public void Awake()
+    {
+        if (sharedInstance != null && sharedInstance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            sharedInstance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetupScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetupScene()
+    {
+        SpawnPlayer();
+    }
+
+    public void SpawnPlayer()
+    {
+        if (playerSpawnPoint != null)
+        {
+            GameObject player = playerSpawnPoint.SpawnObject();
+
+            // Set the virtual camera to follow the player that was just spawned
+            cameraManager.virtualCamera.Follow = player.transform;
+        }
+    }
+}
