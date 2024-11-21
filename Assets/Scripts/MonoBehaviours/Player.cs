d46f37ae13bb61b1a4f1b95c43171ffd14e96e37
Logan Reine <log.a.of.n@gmail.com>
2024-11-20 22:20:31 -0500

    added scripts Consumable, Player, and Item

--- Assets/Scripts/MonoBehaviours/Player.cs
+++ Assets/Scripts/MonoBehaviours/Player.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : Character
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
+        // trigger for any player collisions with consumable items
+        if (collision.gameObject.CompareTag("CanBePickedUp"))
+        {
+            Item hitObject = collision.gameObject.GetComponent<Consumable>().item;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            if (hitObject != null)
+            {
+                // debugging
+                print("it: " + hitObject.objectName);
+
+                collision.gameObject.SetActive(false);
+            }
+        }
     }
 }
