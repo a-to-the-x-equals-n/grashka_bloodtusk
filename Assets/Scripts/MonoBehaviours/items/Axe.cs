846c4bedf7e38d61464cd1ce31685027869745b3
Logan Reine <log.a.of.n@gmail.com>
2024-12-05 18:55:14 -0500

    fixed axe throwing animation

--- Assets/Scripts/MonoBehaviours/items/Axe.cs
+++ Assets/Scripts/MonoBehaviours/items/Axe.cs
@@ -4,27 +4,43 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class Axe : MonoBehaviour
 {
-    // Axe prefab assigned in inspector
-    public GameObject axePrefab;
-    // Is the player currently attacking
-    bool isAttacking;
-    // Checks to see if axe is already thrown
-    bool unarmed = false;
+    // Singleton instance
+    public static Axe Instance { get; private set; }
 
-    // Animator reference
     [HideInInspector] public Animator animator;
-    // reference to the camera
-    Camera localCamera;
-    float positiveSlope, negativeSlope;
+    public GameObject axePrefab;
+    private GameObject axeObject;
+    private Rigidbody2D rb2d;
+    private float positiveSlope, negativeSlope;
+    private Camera localCamera;
+
+    // Enums for directions
+    private enum Quadrant { East, South, West, North }
+    private enum SlopeLine { Positive, Negative }
 
     public float velocity;
-
-    // Enum to describe the direction the player is firing in
-    enum Quadrant { East, South, West, North }
-    enum SlopeLine { Positive, Negative }
-
-    // Amount of damage the axe will inflict on an enemy
     public int damageInflicted;
+    private bool isAttacking;
+    private bool armed = true; // Ensures only one axe can be thrown
+
+    // Called when the script is being loaded
+    private void Awake()
+    {
+        // Singleton setup
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+        animator = GetComponent<Animator>();
+        localCamera = Camera.main;
+
+        // Instantiate the axe object
+        axeObject = Instantiate(axePrefab);
+        axeObject.SetActive(false);
+    }
 
     private void Start()
     {
@@ -44,41 +60,63 @@ public class Axe : MonoBehaviour
         negativeSlope = GetSlope(upperLeft, lowerRight);
     }
 
+    // Called when the gameobject is destroyed
+    private void OnDestroy()
+    {
+        
+    }
+
+    // Called each frame
+    private void Update()
+    {
+        // Handle user input for throwing the axe
+        if (Input.GetMouseButtonDown(0) && armed)
+        {
+            ThrowAxe();
+        }
+        UpdateState();
+    }
+
     // Calculate the slope of a line, given two (x,y) points
     float GetSlope(Vector2 pointOne, Vector2 pointTwo)
     {
         return (pointTwo.y - pointOne.y) / (pointTwo.x - pointOne.x);
     }
 
-    // Called each frame
-    private void Update()
-    {
-        // Check to see if user has clicked the mouse to fire the slingshot
-        if (!unarmed && Input.GetMouseButtonDown(0)) // Parameter 0 checks for left mouse button; 1 checks for right
-        {
-            ThrowAxe();
-        }
-    }
-
     private void ThrowAxe()
     {
+        armed = false;
+
         // Convert the mouse position from screen space to world space
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        // Get a new ammo object located at the weapon's current position
-        Vector3 spawnPosition = transform.position;
-        Quaternion spawnRotation = Quaternion.identity; // No rotation
-        Instantiate(axePrefab, spawnPosition, spawnRotation);
-        axePrefab.SetActive(true);
+        // Enable and set position
+        axeObject.SetActive(true);
+        axeObject.transform.position = transform.position;
 
-        // Get reference to the arc script
-        Arc arcScript = axePrefab.GetComponent<Arc>();
-
-        // Calculate the amount of time for ammo travel
+        if(axeObject != null)
+        {
+            // Calculate the amount of time for ammo travel
             // Example: if velocity is 2, then 1/2 = 0.5 or a half second to travel across the screen
-        float travelDuration = 1.0f / velocity;
+            float travelDuration = 1.0f / velocity;
+            StartCoroutine(AxeArcCycle(mousePosition, travelDuration));
+        }
+    }
 
-        StartCoroutine(arcScript.TravelArc(mousePosition, travelDuration));
+    private IEnumerator AxeArcCycle(Vector3 destination, float duration)
+    {
+        // Get reference to the arc script
+        AxeArc arcScript = axeObject.GetComponent<AxeArc>();
+
+        // Execute the travel arc
+        yield return StartCoroutine(arcScript.TravelArc(destination, duration));
+
+        // Execute the return arc
+        yield return StartCoroutine(arcScript.ReturnArc(transform.position, duration));
+
+        // Deactivate the axe and re-arm
+        axeObject.SetActive(false);
+        armed = true;
     }
 
     private void TriggerAttack()
@@ -86,7 +124,7 @@ public class Axe : MonoBehaviour
         if (!isAttacking)
         {
             isAttacking = true;
-            unarmed = true;
+            armed = false;
 
             Quadrant attackDirection  = GetQuadrant();
             var quadrantVector = attackDirection  switch
@@ -114,7 +152,31 @@ public class Axe : MonoBehaviour
         Debug.Log("Resetting attack");
         isAttacking = false;
         animator.SetBool("isAttacking", false);
-        unarmed = false;
+        armed = true;
+    }
+
+    private void UpdateState()
+    {
+        if (isAttacking)
+        {
+            Quadrant quadEnum = GetQuadrant();
+            var quadrantVector = quadEnum switch
+            {
+                Quadrant.East => new Vector2(1.0f, 0.0f),
+                Quadrant.South => new Vector2(0.0f, -1.0f),
+                Quadrant.West => new Vector2(-1.0f, 1.0f),
+                Quadrant.North => new Vector2(0.0f, 1.0f),
+                _ => new Vector2(0.0f, 0.0f),
+            };
+            animator.SetBool("isAttacking", true);
+            animator.SetFloat("fireXDir", quadrantVector.x);
+            animator.SetFloat("fireYDir", quadrantVector.y);
+            isAttacking = false;
+        }
+        else
+        {
+            animator.SetBool("isAttacking", false);
+        }
     }
 
     // Determines whether the input position is above a given sloped line
